390a8dae6fc6cfa634d8e1ce3436e81e9bc3ebe9
hhegi <larbillilith@gmail.com>
2026-04-13 12:24:57 +0900

    백준 동전 0

---
+++ src/main/java/com/codetest/coinzero.java
@@ -0,0 +1,19 @@
+package com.codetest;
+
+import java.util.Scanner;
+
+public class coinzero {
+    //백준 11047 동전 0
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i, m = 0, n = sc.nextInt(), k = sc.nextInt();
+		int a[] = new int[n + 1];
+		for (i = 1; i <= n; i++) a[i] = sc.nextInt();
+		for(i=n;i>0;i--){
+			m+=k/a[i];
+            k%=a[i];
+		}
+		System.out.println(m);
+        sc.close();
+	}
+}
